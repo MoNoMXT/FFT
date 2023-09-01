9bb0ea91ffbe2fefc060fdd81c7eafd95489b8ff
Alejandro Maxit <87787390+MoNoMXT@users.noreply.github.com>
2023-09-01 10:50:56 -0300

    Update FFT2.m

--- FFT2.m
+++ FFT2.m
@@ -2,17 +2,14 @@ close all
 clear all
 
 % Señal de entrada
-x = [0, 1, 2, 1];
+x = [0, 1, 2, 1]
 N = length(x);
 
 % Cálculo de la FFT
-X_fft = fft(x);
+X_fft = fft(x)
 
 % Frecuencias correspondientes
-frequencias = linspace(0, 1, N);
-% Fracciones del 1 al 10 para el eje x
-fraction_ticks = 1:10;
-fraction_labels = arrayfun(@(x) sprintf('%d/10', x), fraction_ticks, 'UniformOutput', false);
+frequencies = linspace(0, 1, N);
 
 % Gráfico de la señal original
 subplot(3,1,1);
@@ -24,22 +21,23 @@ grid on;
 
 % Gráfico del espectro de amplitud
 subplot(3,1,2);
-stem(frequencias, abs(X_fft), 'r', 'LineWidth', 2);
+stem(frequencies, abs(X_fft), 'r', 'LineWidth', 2);
 title('Espectro de Amplitud (FFT)');
-xlabel('Frecuencia');
+xlabel('Frecuencia (Hz)');
 ylabel('|X(f)|');
-set(gca, 'XTick', fraction_ticks / 10);
-set(gca, 'XTickLabel', fraction_labels);
 grid on;
 
 % Gráfico del espectro de fase
 subplot(3,1,3);
-stem(frequencias, angle(X_fft), 'r', 'LineWidth', 2);
+stem(frequencies, angle(X_fft), 'r', 'LineWidth', 2);
 title('Espectro de Fase (FFT)');
-xlabel('Frecuencia');
+xlabel('Frecuencia (Hz)');
 ylabel('Ángulo (radianes)');
-set(gca, 'XTick', fraction_ticks / 10);
-set(gca, 'XTickLabel', fraction_labels);
 set(gca, 'YTick', [-pi, -pi/2, 0, pi/2, pi]);
 set(gca, 'YTickLabel', {'-π', '-π/2', '0', 'π/2', 'π'});
 grid on;
+
+% Ajustar espacio entre subplots
+subplot_adjust = get(gcf, 'Position');
+subplot_adjust(4) = subplot_adjust(4) - 0.05;
+set(gcf, 'Position', subplot_adjust);
